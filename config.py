16c16aa39e2ae95dbeb5ea47a56358d754a4bbac
n1ckelman <rybkin.k@gmail.com>
2016-03-09 06:45:12 +0200

    fix db uri

--- config.py
+++ config.py
@@ -5,5 +5,8 @@ CSRF_ENABLED = True
 SECRET_KEY = 'you-will-never-guess'
 SEARCH_CHOICES = ['authors', 'books']
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+if os.environ.get('DATABASE_URL') is None:
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+else:
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
